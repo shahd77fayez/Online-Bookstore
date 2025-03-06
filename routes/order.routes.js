34ddcbbc47f0d83b4c18cb7a04ed62620e8c814a
MalakGhazy <gmalak541@gmail.com>
2025-03-06 22:14:22 +0200

    fix : resolve conflicts in import

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -2,7 +2,6 @@ import express from "express";
 import { placeOrder, getOrderHistory } from "../controllers/order.controller.js";
 import { validateRequest } from "../middlewares/ValidateRequest.js";
 import { orderSchema } from "../middlewares/OrderValidation.js";
-import { placeOrder, getOrderHistory } from "../controllers/order.controller.js";
 import { auth } from "../middlewares/auth.js";  
 const router = express.Router();
 router.route("/")
