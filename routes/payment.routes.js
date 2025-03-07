530662f990d3d6d9f5181e6acaecdbfb460cade7
toka shawky <tokaelmaadawy@gmail.com>
2025-03-07 08:46:35 +0200

    fix admin check role

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -2,5 +2,6 @@
 import express from 'express';
 import { createPaymentIntent } from '../controllers/payment.controller.js';  // Import the controller method
 const router = express.Router();
-router.post('/',isAdmin, createPaymentIntent);
+import { checkRole } from "../middlewares/roleCheck.js"; // Role-based check (admin)
+router.post('/',checkRole('Admin'), createPaymentIntent);
 export default router; // Use export default
