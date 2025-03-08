505b79f561b70f13b697c48571f30e9f87b06f44
Shahd77Fayez <Shahd2003fayez@gmail.com>
2025-03-09 00:13:11 +0200

    fix:use authroize instead of checkrole

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import {getOrderHistory, placeOrder, updateOrderStatus} from '../controllers/order.controller.js';
-import {auth} from '../middlewares/auth.js';
 import {checkRole} from '../middlewares/roleCheck.js'; // Role-based check (admin)
 import {validateRequest} from '../middlewares/ValidateRequest.js';
 import {orderSchema} from '../validation/OrderValidation.js'; // Adjusted casing
+import {asyncHandler} from '../middlewares/ErrorHandling.js';
+import {auth, roles} from '../middlewares/auth.js';
+import {authorize} from '../middlewares/authorize.js';
 
 const router = express.Router();
 
 router.route('/')
   .post(auth(), validateRequest(orderSchema), placeOrder)
-  .get(auth(), getOrderHistory);
+  .get(auth(), asyncHandler(getOrderHistory));
 
 router.route('/:orderId/status')
-  .patch(auth(), checkRole('Admin'), updateOrderStatus);
+  .patch(auth(), authorize(roles.admin), asyncHandler(updateOrderStatus));
 export default router; // Correct export here
--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -2,7 +2,10 @@
 import express from 'express';
 import {createPaymentIntent} from '../controllers/payment.controller.js';
 import {checkRole} from '../middlewares/roleCheck.js';
+import {auth, roles} from '../middlewares/auth.js';
+import {authorize} from '../middlewares/authorize.js';
+import {asyncHandler} from '../middlewares/ErrorHandling.js';
 // Import the controller method
 const router = express.Router(); // Role-based check (admin)
-router.post('/', checkRole('Admin'), createPaymentIntent);
+router.post('/', auth(),authorize(roles.admin), asyncHandler(createPaymentIntent));
 export default router; // Use export default
